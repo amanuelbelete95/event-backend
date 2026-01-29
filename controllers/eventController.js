21f7cfddd5672db4f916ad52c15ec0ffd502a470
Amanuel Belete <amanuelbelete95@gmail.com>
2026-01-29 16:38:40 +0300

    fix db import issue

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,138 +1,147 @@
+import pool from '../db.js';
+
 export const createEvent = async (req, res) => {
-    try {
-        const { name, location, event_date, event_status, description } = req.body;
-        if (!name || !location || !event_date || !event_status) {
-            return res.status(400).json({
-                message: 'Bad Request: All fields are required.',
-                code: 400,
-            });
-        }
-        const newEvent = await pool.query(
-            `insert into event (name,location,event_date,event_status,description)  
+  try {
+    const { name, location, event_date, event_status, description } = req.body;
+    if (!name || !location || !event_date || !event_status) {
+      return res.status(400).json({
+        message: 'Bad Request: All fields are required.',
+        code: 400,
+      });
+    }
+    const newEvent = await pool.query(
+      `insert into event (name,location,event_date,event_status,description)  
        values ($1, $2, $3, $4, $5)
        returning *
       `,
-            [name, location, event_date, event_status, description]
-        );
-        res.json(newEvent.rows[0]);
-    } catch (error) {
-        return res.status(500).json({
-            message: 'Internal Server Error: An unexpected error occurred.',
-            code: 500,
-        });
-    }
-}
+      [name, location, event_date, event_status, description]
+    );
+    res.json(newEvent.rows[0]);
+  } catch (error) {
+    return res.status(500).json({
+      message: 'Internal Server Error: An unexpected error occurred.',
+      code: 500,
+    });
+  }
+};
 // get All Events
 
 export const getallEvents = async (req, res) => {
-    try {
-        const allEvents = await pool.query(`select * from event`);
-        if (allEvents.rows.length === 0) {
-            return res.status(404).json({
-                message: 'No events found.',
-                code: 404,
-            });
-        }
-        res.json(allEvents.rows);
-    } catch (error) {
-        return res.status(500).json({
-            message: 'Internal Server Error: An unexpected error occurred.',
-            code: 500,
-        });
+  try {
+    const allEvents = await pool.query(`select * from event`);
+    if (allEvents.rows.length === 0) {
+      return res.status(404).json({
+        message: 'No events found.',
+        code: 404,
+      });
     }
-}
+    res.json(allEvents.rows);
+  } catch (error) {
+    return res.status(500).json({
+      message: 'Internal Server Error: An unexpected error occurred.',
+      code: 500,
+    });
+  }
+};
 
 // get single event
 
 export const getSingleEvent = async (req, res) => {
-    try {
-        const { event_id } = req.params;
-        if (!event_id) {
-            return res.status(400).json({
-                message: 'Bad Request: Event ID is required.',
-                code: 400,
-            });
-        }
-
-        const getEvent = await pool.query(
-            `select * from event where event_id = $1`,
-            [event_id]
-        );
-
-        if (getEvent.rows.length === 0) {
-            return res.status(404).json({
-                message: `Event with ID ${event_id} not found.`,
-                code: 404,
-            });
-        }
-
-        res.status(200).json(getEvent.rows[0]);
+  try {
+    const { event_id } = req.params;
+    if (!event_id) {
+      return res.status(400).json({
+        message: 'Bad Request: Event ID is required.',
+        code: 400,
+      });
     }
-    catch (error) {
-        return res.status(500).json({
-            message: 'Internal Server Error: An unexpected error occurred.',
-            code: 500,
-        });
+
+    const getEvent = await pool.query(
+      `select * from event where event_id = $1`,
+      [event_id]
+    );
+
+    if (getEvent.rows.length === 0) {
+      return res.status(404).json({
+        message: `Event with ID ${event_id} not found.`,
+        code: 404,
+      });
     }
-}
+
+    res.status(200).json(getEvent.rows[0]);
+  } catch (error) {
+    return res.status(500).json({
+      message: 'Internal Server Error: An unexpected error occurred.',
+      code: 500,
+    });
+  }
+};
 
 // Update an event
 export const updateEvent = async (req, res) => {
-    try {
-        const { event_id } = req.params;
-        if (!event_id) {
-            return res.status(400).json({
-                message: 'Bad Request: Event ID is required.',
-                code: 400,
-            });
-        }
-        const { name, location } = req.body;
+  try {
+    const { event_id } = req.params;
+    if (!event_id) {
+      return res.status(400).json({
+        message: 'Bad Request: Event ID is required.',
+        code: 400,
+      });
+    }
+    const { name, location } = req.body;
 
-        if (!name || !location) {
-            return res.status(404).json({
-                message: 'Bad Request: Event name and location are required.',
-                code: 400,
-            });
-        }
-        const updatedEvent = await pool.query(
-            `update event 
+    if (!name || !location) {
+      return res.status(404).json({
+        message: 'Bad Request: Event name and location are required.',
+        code: 400,
+      });
+    }
+    const updatedEvent = await pool.query(
+      `update event 
        set name = $1,location = $2
        where event_id = $3
        returning
        *
       `,
-            [name, location, event_id]
-        );
-        res.json(updatedEvent.rows[0]);
-    } catch (error) {
-        return res.status(500).json({
-            message: 'Internal Server Error: An unexpected error occurred.',
-            code: 500,
-        });
-    }
-}
+      [name, location, event_id]
+    );
+    res.json(updatedEvent.rows[0]);
+  } catch (error) {
+    return res.status(500).json({
+      message: 'Internal Server Error: An unexpected error occurred.',
+      code: 500,
+    });
+  }
+};
 
 export const deleteEvent = async (req, res) => {
-    try {
-        const { event_id } = req.params;
-        if (!event_id) {
-            return res.status(400).json({
-                message: 'Bad Request: Event ID is required.',
-                code: 400,
-            });
-        }
-        const deletedEvent = await pool.query(
-            `
+  try {
+    const { event_id } = req.params;
+    if (!event_id) {
+      return res.status(400).json({
+        message: 'Bad Request: Event ID is required.',
+        code: 400,
+      });
+    }
+    const deletedEvent = await pool.query(
+      `
        delete from event 
        where event_id = $1
        returning
          *
       `,
-            [event_id]
-        );
-        return res.json({ message: 'Event deleted successfully', event: deletedEvent.rows[0] });
-    } catch (error) {
-        console.error('Error deleting event:', error.message);
-        return res.status(500).json({ message: 'Internal Server Error: An unexpected error occurred.', code: 500 });
-    }
-}
+      [event_id]
+    );
+    return res.json({
+      message: 'Event deleted successfully',
+      event: deletedEvent.rows[0],
+    });
+  } catch (error) {
+    console.error('Error deleting event:', error.message);
+    return res
+      .status(500)
+      .json({
+        message: 'Internal Server Error: An unexpected error occurred.',
+        code: 500,
+      });
+  }
+};
